4daf8b736840e0874eba2d58969340130c74fc2a
pett <35666826+Pettantva@users.noreply.github.com>
2018-02-26 22:51:44 +0100

    aef

---
+++ Assignment_3/Assignment_3/src/model/grid.java
@@ -0,0 +1,16 @@
+package model;
+
+public class grid {
+	private int head;
+	private int[][][] grid;
+	
+	public grid(int rows, int cols, int head){
+		this.head = head;
+		this.grid = new int[rows][cols][4];
+		setGrid();
+	}
+	
+	private void setGrid(){
+
+	}
+}
